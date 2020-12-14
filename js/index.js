27efa7f4c9002ded0a8cbc97b265bc0bc414ac71
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2020-12-15 07:50:30 +0800

    update

--- js/index.js
+++ js/index.js
@@ -50,3 +50,87 @@ function showSub(){
 
 }
 
+// SLIDESHOW FOR ADVERTISEMENT //
+
+
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+      // Apply to all slideshows that you define with the markup wrote
+  slideshows.forEach(initSlideShow);
+
+  function initSlideShow(slideshow) {
+
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+    var index = 0, time = 3000;
+    slides[index].classList.add('active');  
+    
+    setInterval( () => {
+      slides[index].classList.remove('active');
+      
+      //Go over each slide incrementing the index
+      index++;
+      
+      // If you go over all slides, restart the index to show the first slide and start again
+      if (index === slides.length) index = 0; 
+      
+      slides[index].classList.add('active');
+
+    }, time);
+  }
+
+//END OF SLIDESHOW//
+
+// ANIMATION SLIDE FOR TIMELINE //
+
+$(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+        
+        sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+  
+  });
+  
