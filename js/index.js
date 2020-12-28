97d1205bec2bc449aba98aa554cd8edc25119b9d
osirisgenius <59709335+osirisgenius@users.noreply.github.com>
2020-12-29 04:00:34 +0800

    update rushhh
    need to add onscroll on menu toggler

--- js/index.js
+++ js/index.js
@@ -14,41 +14,83 @@ $(function(){
           origin: 'right',
           distance: '300px',
           easing: 'ease-in-out',
-          duration: 800,
+          duration: 1000,
+        });
+
+
+        sr.reveal('.js--fadeInLeft1', {
+          origin: 'left',
+          distance: '1000px',
+            easing: 'ease-in-out',
+          duration: 1000,
+        });
+
+        sr.reveal('.js--fadeInLeft2', {
+          origin: 'left',
+          distance: '600px',
+            easing: 'ease-in-out',
+          duration: 1000,
+         });
+
+
+        sr.reveal('.js--fadeInLeft3', {
+          origin: 'left',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 1000,
         });
   
     } else {
         
-        sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    }
-    
     sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 1000,
+    });
+
+    sr.reveal('.js--fadeInLeft1', {
           origin: 'left',
+          distance: '1000px',
+            easing: 'ease-in-out',
+          duration: 1500,
+        });
+
+    sr.reveal('.js--fadeInLeft2', {
+          origin: 'left',
+          distance: '600px',
+            easing: 'ease-in-out',
+          duration: 2000,
+    });
+
+
+    sr.reveal('.js--fadeInLeft3', {
+      origin: 'left',
+      distance: '300px',
+        easing: 'ease-in-out',
+      duration: 3000,
+    });
+        sr.reveal('.js--fadeup', {
+          origin: 'top',
           distance: '300px',
             easing: 'ease-in-out',
-          duration: 800,
+          duration: 2000,
         });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
+
+        sr.reveal('.js--fadeDown', {
+          origin: 'bottom',
           distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
+            easing: 'ease-in-out',
+          duration: 1000,
         });
-  
+      }
   
   });
   
@@ -56,16 +98,56 @@ $(function(){
 
   $(document).ready(function() {
     $(".milestone-show").click(function () {
-    $(".timehide").show()
-    $(".milestone-show").hide()
-    $(".milestone-hide").show()
+    $(".timehide").show(1000)
+    $(".milestone-show").slideToggle("slow");
+    $(".milestone-hide").slideToggle("slow");
  });
  });
 
  $(document).ready(function() {
   $(".milestone-hide").click(function () {
-  $(".timehide").hide()
-  $(".milestone-show").show()
-  $(".milestone-hide").hide()
+  $(".timehide").hide(1000)
+  $(".milestone-show").slideToggle("slow");
+  $(".milestone-hide").slideToggle("slow");
 });
+});
+
+
+// NAVMENU FOR MOBILE
+
+$(document).ready(function(){
+  $(".menu-toggler").click(function(){
+    
+    if ($(".fa-bars").hasClass("fa-bars")){
+
+    $(".navbar").slideToggle("slow");
+    $(".fa-bars").addClass("fa-close");
+    $(".fa-bars").removeClass(" fa-bars");}
+
+    else {
+
+    $(".navbar").slideToggle("slow");
+    $(".fa-close").addClass("fa-bars");
+    $(".fa-close").removeClass("fa-close");}
+
+  });
+});
+
+//NAVMENU SCROLL
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $("menu-toggler").animate({backgroundColor: 'rgba(255,255,255,.9)'}, 1000);
+    }
+  });
+});
+
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+    $(".cwhite").addClass("cblue");
+    $(".cwhite").removeClass(" cwhite");}
+  }
+  });
 });
